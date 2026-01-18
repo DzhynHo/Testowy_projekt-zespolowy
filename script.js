df510bbf907b2f5e9670fd597961ca85f90a8b37
Yana Trotsenko <164183176+yunayana@users.noreply.github.com>
2026-01-18 21:04:14 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("myButton");
-const text = document.getElementById("text");
-
-button.addEventListener("click", () => {
-    text.textContent = "JavaScript działa! 🔥";
-});
