e9516daddd40243bdfd4ac624e6ef4bdef28e0c6
DzhynHo <epfkhylchenko1321@gmail.com>
2026-01-18 14:59:53 +0100

    Dodanie plików html i js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Pobranie przycisku po ID
+const button = document.getElementById("myButton");
+
+// Funkcja wywoływana po kliknięciu
+button.addEventListener("click", function() {
+    alert("Cześć! Kliknąłeś przycisk!");
+});
